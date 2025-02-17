5f280efcbb317ce6230d2e3fc372254014c23d65
agent-52 <abhaybhadauriya52@gmail.com>
2025-02-18 03:14:53 +0530

    fix file locations in import'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Header from "../Components/Header"
-import Section1 from "../Components/Section1"
-import Section2 from "../Components/Section2"
-import Section3 from "../Components/Section3"
-import Section4 from "../Components/Section4"
-import Footer from "../Components/Footer"
-import FormSection from "../Components/FormSection"
+import Header from "./Components/Header"
+import Section1 from "./Components/Section1"
+import Section2 from "./Components/Section2"
+import Section3 from "./Components/Section3"
+import Section4 from "./Components/Section4"
+import Footer from "./Components/Footer"
+import FormSection from "./Components/FormSection"
 
 function App() {
   
