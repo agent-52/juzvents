624403fec00e1d32e6bfd4f5c7e8724a1689849a
agent-52 <abhaybhadauriya52@gmail.com>
2025-03-06 07:38:37 +0530

    connect form to whatsapp and fix its overflow in mobiles

--- src/Components/FormSection.jsx
+++ src/Components/FormSection.jsx
@@ -1,6 +1,21 @@
 import Button from "../Components/Button"
+import { useState } from "react"
 
 const FormSection = () =>{
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] = useState("")
+    const [phone, setPhone] = useState("")
+    const [email, setEmail] = useState("")
+    const [customMessage, setCustomMessage] = useState("")
+    const sendWhatsAppMessage = () =>{
+        let firstname = document.querySelector("#firstname").value.trim()
+        let lastname = document.querySelector("#lastname").value.trim()
+        let customMessage = document.querySelector("#customMessage").value.trim()
+        let message = `Hello, ${firstname} this side. ${customMessage}, Looking forward to your response. Thank you!`;
+        let phoneNumber = "+919244330496"; // Change this to your number
+        let url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        window.open(url, "_blank");
+    }
 
     return(
         <section className="flex justifyC gap6 pdb6 pdt8 formSection">
@@ -12,29 +27,29 @@ const FormSection = () =>{
                 </div>
             </div>
             <div>
-                <form action="" method="post" className="flexC gap1_5">
+                <form  onSubmit={sendWhatsAppMessage} className="flexC gap1_5">
                     <h4 className="pdB2 fs2 mediumF">Connect with us</h4>
                     <div className="flex gap1_5 nameSection">
                         <div className="flexC gap0">
-                            <label htmlFor="firstName">First Name</label>
-                            <input type="text" className="input"/>
+                            <label htmlFor="firstName">First Name*</label>
+                            <input type="text" className="input" value={firstname}  onChange={(firstname) => setFirstname(firstname.target.value)} id="firstname" required/>
                         </div>
                         <div className="flexC gap0">
                             <label htmlFor="lastName">Last Name</label>
-                            <input type="text" className="input"/>
+                            <input type="text" className="input" value={lastname}  onChange={(lastname) => setLastname(lastname.target.value)} id="lastname"/>
                         </div>
                     </div>
                     <div className="flexC gap0">
-                        <label htmlFor="email">Email*</label>
-                        <input type="text" className="input"/>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" className="input" value={email}  onChange={(email) => setEmail(email.target.value)} id="email"/>
                     </div>
                     <div className="flexC gap0">
                         <label htmlFor="phone">Phone*</label>
-                        <input type="text" className="input"/>
+                        <input type="tel" className="input" value={phone}  onChange={(phone) => setPhone(phone.target.value)} id="phone"/>
                     </div>
                     <div className="flexC gap0">
                         <label htmlFor="message">Write a message</label>
-                        <textarea />
+                        <textarea value={customMessage}  onChange={(customMessage) => setCustomMessage(customMessage.target.value)} id="customMessage"/>
                     </div>
                     <div className="pdb1 formButton">
                         <Button text="Submit" classArray="bgW cD fs1 regularF pdi3 pdb0_5 brR"/>
